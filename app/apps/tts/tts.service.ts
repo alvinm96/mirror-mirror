63def9aeebed7eb314db1105ed59abf4521d69e1
Alvin <alvinm@voicebox.com>
2016-10-24 12:08:06 -0700

    Refactor TTS

--- app/apps/tts/tts.service.ts
+++ app/apps/tts/tts.service.ts
@@ -5,20 +5,27 @@ import { config } from './../../config';
 export class TtsService {
   private key: string = config.voicebox.key;
   private voice: string = config.voicebox.ttsVoice;
-  voices: Object[] = [];
-  synthesize: any;
-  window;
-  options: Object;
 
-  constructor() { 
-    this.window = window;
-    this.options = {
-      voice: this.voice,
-      key: this.key
-    };
-  }
+  constructor() { }
 
   synthesizeSpeech(text: string) {
-    this.synthesize = this.window.tts(this.options, text);
+    let url = 'https://api.voicebox.com/speech-synthesis/v1/synthesize';
+    let xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function() {
+      if (xhttp.readyState == 4 && xhttp.status == 200) {
+        var audio = new Audio();
+        var blobUrl = URL.createObjectURL(xhttp.response);
+        audio.src = blobUrl;
+        audio.play();
+      }
+    };
+    var synthesizeQuery = url +
+      '?text=' + encodeURIComponent(text) + 
+      '&voice=' + this.voice;
+    xhttp.open('GET', synthesizeQuery, true);
+    xhttp.setRequestHeader('Authorization', 'Basic ' + this.key);
+    xhttp.responseType = 'blob';
+    xhttp.send();  
   }
 }
